c8ce2c0244d4a52c6488f1effe1b5d3a7a1433df
Lumikin <lumikomio04@gmail.com>
2025-09-01 15:05:02 -0300

    Atividade 1 feita

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express();
+const PORT = 8081
+const fs = require("fs")
+
+app.get('/usuarios/', (req, res) => {
+    try {
+        const data = fs.readFileSync('./users.json', 'utf-8')
+        let usuarios = JSON.parse(data)
+
+        res.status(200).json(usuarios)
+    } catch (error) {
+        console.error("Algum erro no servidor! Erro: ", error)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em https://localhost:${PORT}`)
+});
